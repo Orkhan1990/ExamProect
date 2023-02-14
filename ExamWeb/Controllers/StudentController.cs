4dc3bc5c62d430bcc556f3f903a7f46562061a05
Orkhan Talibli <orkhan.talibli@gmail.com>
2023-02-14 23:26:15 +0400

    third commit

--- ExamWeb/Controllers/StudentController.cs
+++ ExamWeb/Controllers/StudentController.cs
@@ -32,6 +32,75 @@ namespace ExamWeb.Controllers
         public async Task<IActionResult> Create(Student student)
         {
 
+            var validationResult = await _validator.ValidateAsync(student);
+            if (validationResult.IsValid)
+            {
+                Student newStudent = new Student();
+                newStudent.Name = student.Name;
+                newStudent.Surname = student.Surname;
+                await _studentService.CreateAsync(newStudent);
+                return RedirectToAction("Index", "Student");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                            
+            }
+
+            return View();
+        }
+
+       
+
+        public async Task<IActionResult>DeleteStudent(int id)
+        {
+            var entity =await _studentService.GetById(id);
+            if (entity == null)
+            {
+                throw new ArgumentException("Belə bir şagird sistemdə yoxdur!");
+            }
+
+            await _studentService.DeleteAsync(entity);
+            return RedirectToAction("Index", "Student");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult>UpdateStudent(int id)
+        {
+            var entity=await _studentService.GetById(id);
+            if (entity == null)
+            {
+                throw new ArgumentException("Belə bir şagird sistemdə yoxdur!");
+            }
+
+
+            return View(entity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateStudent(Student student)
+        {
+            var validationResult = await _validator.ValidateAsync(student);
+            if (validationResult.IsValid)
+            {
+                //Student newStudent = new Student();
+                //newStudent.Name = student.Name;
+                //newStudent.Surname = student.Surname;
+                await _studentService.UpdateAsync(student);
+                return RedirectToAction("Index", "Student");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+
+            }
+
             return View();
         }
     }
